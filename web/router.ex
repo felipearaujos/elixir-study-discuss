69c3b9cdf63fe9f79c19cb2d24131cca16c88de2
Felipe Araujo <felipe.araujo.s@live.com>
2020-01-18 17:13:31 -0300

    ueberauth github module

--- web/router.ex
+++ web/router.ex
@@ -27,6 +27,15 @@ defmodule Discuss.Router do
     
   end
 
+  scope "/auth", Discuss do
+    pipe_through :browser # Use the default browser stack
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+    
+  end
+
+
   # Other scopes may use custom stacks.
   # scope "/api", Discuss do
   #   pipe_through :api
